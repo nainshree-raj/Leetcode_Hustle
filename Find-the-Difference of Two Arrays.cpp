b59a342b642e6a537e65e22b230bd2bb0323d3d6
Nainshree Raj <71694916+nainshree-raj@users.noreply.github.com>
2022-08-18 20:00:27 +0530

    Update Find-the-Difference of Two Arrays.cpp

--- Find-the-Difference of Two Arrays.cpp
+++ Find-the-Difference of Two Arrays.cpp
@@ -18,3 +18,30 @@ Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
 Constraints:
 1 <= nums1.length, nums2.length <= 1000
 -1000 <= nums1[i], nums2[i] <= 1000
+
+ Answer: class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+     unordered_set<int>s1(nums1.begin(),nums1.end());
+     unordered_set<int>s2(nums2.begin(),nums2.end());  
+        vector<int>res1,res2;
+        vector<vector<int>>res;
+        for(auto i:s1)
+        {
+            if(s2.find(i)==s2.end())
+            {
+                res1.push_back(i);
+            }
+        }
+         for(auto i:s2)
+        {
+            if(s1.find(i)==s1.end())
+            {
+                res2.push_back(i);
+            }
+        }
+        res.push_back(res1);
+        res.push_back(res2);
+        return res;
+    }
+};
