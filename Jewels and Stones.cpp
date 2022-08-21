872ab25f15bd345b75e7947d5bbf365350cace0a
Nainshree Raj <71694916+nainshree-raj@users.noreply.github.com>
2022-08-21 19:31:45 +0530

    Update Jewels and Stones.cpp

--- Jewels and Stones.cpp
+++ Jewels and Stones.cpp
@@ -10,6 +10,11 @@ Example 2:
 Input: jewels = "z", stones = "ZZ"
 Output: 0
   
+Constraints:
+->1 <= jewels.length, stones.length <= 50
+->jewels and stones consist of only English letters.
+->All the characters of jewels are unique.
+  
 SOLUTION:
 class Solution {
 public:
