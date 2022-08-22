68cc8146c71d64110046166734463427b2a5c5d5
Nainshree Raj <71694916+nainshree-raj@users.noreply.github.com>
2022-08-22 21:03:10 +0530

    Update Find-the-Difference of Two Arrays.cpp

--- Find-the-Difference of Two Arrays.cpp
+++ Find-the-Difference of Two Arrays.cpp
@@ -1,5 +1,6 @@
 
-QUESTION :Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+QUESTION :
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
 
 answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
 answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
