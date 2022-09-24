9eed4e2c97187f3f0676567509890cefa36e8a50
Nainshree Raj <71694916+nainshree-raj@users.noreply.github.com>
2022-09-24 12:03:34 +0530

    Create Longest Subsequence With Limited Sum.cpp

---
+++ Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,52 @@
+QUESTION :
+You are given an integer array nums of length n, and an integer array queries of length m.
+Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+
+Input: nums = [4,5,2,1], queries = [3,10,21]
+Output: [2,3,4]
+Explanation: We answer the queries as follows:
+- The subsequence [2,1] has a sum less than or equal to 3. It can be proven that 2 is the maximum size of such a subsequence, so answer[0] = 2.
+- The subsequence [4,5,1] has a sum less than or equal to 10. It can be proven that 3 is the maximum size of such a subsequence, so answer[1] = 3.
+- The subsequence [4,5,2,1] has a sum less than or equal to 21. It can be proven that 4 is the maximum size of such a subsequence, so answer[2] = 4.
+  
+SOLUTION :
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        vector<int> ans;
+        int m=queries.size();
+        int n=nums.size();
+        int prefix[n+1];
+        prefix[0]=0;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=1;i<=nums.size();i++)
+        {
+            prefix[i]=prefix[i-1]+nums[i-1];
+        }
+        for(int i=1;i<=n;i++)
+        {
+            cout<<prefix[i];
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(queries[i]<prefix[j])
+                {
+                    ans.push_back(j-1);
+                    break;
+                }
+                if(queries[i]>=prefix[n])
+                {
+                    ans.push_back(n);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
