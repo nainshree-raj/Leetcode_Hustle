c351344b075e2078d4e04d2f8b2af43fb2692fe1
Nainshree Raj <71694916+nainshree-raj@users.noreply.github.com>
2022-10-03 20:45:02 +0530

    Create Kth Largest Element in an Array.cpp

---
+++ Kth Largest Element in an Array.cpp
@@ -0,0 +1,31 @@
+QUESTION :
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+You must solve it in O(n) time complexity.
+  
+SOLUTION :
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++; 
+        }    
+        int ans=0;
+        for(auto it=mp.rbegin();it!=mp.rend();it++)
+        {
+            k=k-(it->second);
+            cout<<k<<endl;
+            if(k<=0)
+            {
+                ans=it->first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+//rbegin & rend = to iterate from end, we use this.
